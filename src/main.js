70151daea185a25e44f7290ffb42902ebb363907
Narendra Sisodiya <narendra@narendrasisodiya.com>
2016-09-28 16:50:15 -0700

    Initial Setup !!

--- src/main.js
+++ src/main.js
@@ -11,12 +11,23 @@ var converter = new HTMLtoJSX({
   createClass: true,
   outputClassName: "AwesomeComponent"
 });
-var input = "<div>Hello world from HTML!</div>";
-var output = converter.convert(input);
+var inputStyle = "<style>\
+  .red {\
+  color: red;\
+}\
+</style>\
+";
+
+var inputDom = "<div>\
+  <p class=\"red\">This is a App comppnent</p>\
+  <p>This is not red</p>\
+</div>";
+
+var output = converter.convert(inputDom);
 var output1 = Babel.transform(output, {presets: ["es2015", "react"]}).code;
 var CHAR_TO_DELETE = 15;
 output1 = output1.substring(CHAR_TO_DELETE) + "window.AwesomeComponent = AwesomeComponent;";
-var output2 = eval(output1);
+eval(output1);
+console.log(output1);
 ReactDom.render(<AwesomeComponent/>, document.getElementById("root"));
-console.log(window.AwesomeComponent, output2);
 
